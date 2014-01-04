3fce3159c9fcbd1703130bae8321c93f6c5c6155
iskey <iskey@outlook.com>
2014-01-04 21:18:59 +0800

    g_list_foreach().

--- glib/GList.c
+++ glib/GList.c
@@ -1,6 +1,11 @@
 #include "glib.h"
 #include "stdio.h"
 
+void my_print(gpointer data_ptr, gpointer usr_data)
+{
+	g_print("%s\n", (gchar *)data_ptr);
+}
+
 int main(int argc, char *argv[])
 {
 	GList *list=NULL;
@@ -12,10 +17,16 @@ int main(int argc, char *argv[])
     ele_num= g_list_length(list);
     printf("The number of list is %d\n", ele_num);
 
+    GList *find_list= g_list_find(list, "hello iskey");
+    gint index= g_list_position(list, find_list);
+    printf("The index of \"hello iskey\" is %d\n", index);
+
     GSList *s_list= NULL;
     s_list= g_slist_append(s_list, "hello world");
     s_list= g_slist_append(s_list, "hello iskey");
     printf("The last element in slist is %s\n", g_slist_last(s_list)->data);
 
+
+    g_list_foreach(list, (GFunc)my_print, NULL);
     exit(0);
 }
